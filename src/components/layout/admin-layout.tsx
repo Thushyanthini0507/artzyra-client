9c66b07bbb47c4e97e5a81b192f1f2e3eb1c0967
Thushyanthini0507 <thushyanthini0507@gmail.com>
2025-12-14 13:09:47 +0530

    refactor: remove PublicNavbar from AdminLayout

--- src/components/layout/admin-layout.tsx
+++ src/components/layout/admin-layout.tsx
@@ -1,12 +1,8 @@
 "use client";
 
-import { PublicNavbar } from "@/components/layout/public-navbar";
-
 export function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col">
-      <PublicNavbar />
-      
       <main className="flex-1 p-8">
         {children}
       </main>
